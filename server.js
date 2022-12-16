2414a6403f1fe8778214c64e3cf6cc3d06980668
Isaac Tsibu-Darko <isaactsibudarko@gmail.com>
2022-12-16 11:17:21 +0000

    worked on postbuild script for render

--- server.js
+++ server.js
@@ -45,11 +45,12 @@ app.use('/api/auth', auth);
 //Serve static assets when in production
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static('./client/build'));
+    //app.use(express.static('./client/build'));
 
-    app.get('*', (req,res) =>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+    //app.get('*', (req,res) =>{
+        //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    //})
+    console.log('we are in production');
 }
 
 const PORT = process.env.PORT || 5000;
